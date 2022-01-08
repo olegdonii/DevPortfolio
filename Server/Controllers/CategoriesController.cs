acad8c80d805816b1a70a5d36810509d851efdca
Oleg Donii <olegdonii@gmail.com>
2022-01-08 21:16:08 +0200

    Creating CategoriesController

---
+++ Server/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Shared.Models;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public CategoriesController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategories()
+        {
+            List<Category> categories = await _db.Categories.ToListAsync();
+            return Ok(categories);
+        }
+    }
+}

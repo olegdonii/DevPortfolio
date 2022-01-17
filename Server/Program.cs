8b2e2cf518e7dd1175831066b860b097788e2075
Oleg Donii <olegdonii@gmail.com>
2022-01-17 15:04:42 +0200

    Changing AllowMethods

--- Server/Program.cs
+++ Server/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
         {
             builder.AllowAnyOrigin()
                    .AllowAnyHeader()
-                   .AllowAnyMethod();
+                   .WithMethods("PUT", "DELETE", "GET");
         });
 });
 

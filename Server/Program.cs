ef468d9710ce3852b589470018592e8f158aa6d7
Oleg Donii <olegdonii@gmail.com>
2022-01-17 18:25:10 +0200

    Moving .db to another folder

--- Server/Program.cs
+++ Server/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
         {
             builder.AllowAnyOrigin()
                    .AllowAnyHeader()
-                   .WithMethods("PUT", "DELETE", "GET");
+                   .AllowAnyMethod();
         });
 });
 

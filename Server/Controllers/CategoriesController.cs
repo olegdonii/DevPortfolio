45191d54c9714d6eeba42cbe513dc9d183b9bac9
Oleg Donii <olegdonii@gmail.com>
2022-02-17 14:27:30 +0200

    Adding new endpoints for getting categories with posts

--- Server/Controllers/CategoriesController.cs
+++ Server/Controllers/CategoriesController.cs
@@ -18,11 +18,75 @@ namespace Server.Controllers
             _db = db;
         }
 
+        // GET api/categories
         [HttpGet]
         public async Task<IActionResult> GetCategories()
         {
             List<Category> categories = await _db.Categories.ToListAsync();
             return Ok(categories);
         }
+
+        // GET api/categories/withposts
+        [HttpGet("withposts")]
+        public async Task<IActionResult> GetWithPosts()
+        {
+            List<Category> categories = await _db.Categories
+                .Include(category => category.Posts)
+                .ToListAsync();
+
+            return Ok(categories);
+        }
+
+        // GET api/categories/1
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            Category category = await GetCategoryByCategoryId(id, false);
+
+            return Ok(category);
+        }
+
+        // GET api/categories/1
+        [HttpGet("withposts/{id}")]
+        public async Task<IActionResult> GetWithPosts(int id)
+        {
+            Category category = await GetCategoryByCategoryId(id, true);
+
+            return Ok(category);
+        }
+
+        #region Utility methods
+
+        [NonAction]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        private async Task<bool> PersistChangesToDatabase()
+        {
+            int amountOfChanges = await _db.SaveChangesAsync();
+
+            return amountOfChanges > 0;
+        }
+
+        [NonAction]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        private async Task<Category> GetCategoryByCategoryId(int categoryId, bool withPosts)
+        {
+            Category categoryToGet = null;
+
+            if (withPosts == true)
+            {
+                categoryToGet = await _db.Categories
+                    .Include(category => category.Posts)
+                    .FirstAsync(category => category.CategoryId == categoryId);
+            }
+            else
+            {
+                categoryToGet = await _db.Categories
+                    .FirstAsync(category => category.CategoryId == categoryId);
+            }
+
+            return categoryToGet;
+        }
+
+        #endregion
     }
 }

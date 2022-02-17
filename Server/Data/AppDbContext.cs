cc21c17cc91fc64dfcf5cd2592a3d9a3c462b593
Oleg Donii <olegdonii@gmail.com>
2022-02-17 13:59:40 +0200

    Updating the database with Post

--- Server/Data/AppDbContext.cs
+++ Server/Data/AppDbContext.cs
@@ -6,12 +6,15 @@ namespace Server.Data
     public class AppDbContext : DbContext
     {
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            #region Categories seed
 
             Category[] categoriesToSeed = new Category[3];
 
@@ -27,6 +30,74 @@ namespace Server.Data
             }
 
             modelBuilder.Entity<Category>().HasData(categoriesToSeed);
+
+            #endregion
+
+            modelBuilder.Entity<Post>(
+                entity => 
+                {
+                    entity.HasOne(post => post.Category)
+                    .WithMany(category => category.Posts)
+                    .HasForeignKey("CategoryId");
+                }
+            );
+
+            #region Posts seed
+
+            Post[] postToSeed = new Post[6];
+
+            for (int i = 1; i < 7; i++)
+            {
+                string postTitle = string.Empty;
+                int categoryId = 0;
+
+                switch (i)
+                {
+                    case 1:
+                        postTitle = "First post";
+                        categoryId = 1;
+                        break;
+                    case 2:
+                        postTitle = "Second post";
+                        categoryId = 2;
+                        break;
+                    case 3:
+                        postTitle = "Third post";
+                        categoryId = 3;
+                        break;
+                    case 4:
+                        postTitle = "Fourth post";
+                        categoryId = 1;
+                        break;
+                    case 5:
+                        postTitle = "Fifth post";
+                        categoryId = 2;
+                        break;
+                    case 6:
+                        postTitle = "Six post";
+                        categoryId = 3;
+                        break;            
+                    default:
+                        break;
+                }
+
+                postToSeed[i - 1] = new Post
+                {
+                    PostId = i,
+                    ThumbnailImagePath = "uploads/placeholder.jpg",
+                    Title = postTitle,
+                    Excerpt = string.Empty,
+                    Content = string.Empty,
+                    PublishDate = DateTime.UtcNow.ToString("dd/MM/yyyy hh:mm"),
+                    Published = true,
+                    Author = "Oleg Donii",
+                    CategoryId = categoryId
+                };
+            }
+                
+            #endregion
+
+            modelBuilder.Entity<Post>().HasData(postToSeed);
         }
     }
 }
---
+++ Server/Data/Migrations/20220217114700_AddedThePostModel.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Data.Migrations
+{
+    public partial class AddedThePostModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    ThumbnailImagePath = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                    Excerpt = table.Column<string>(type: "TEXT", maxLength: 512, nullable: false),
+                    Content = table.Column<string>(type: "TEXT", maxLength: 65536, nullable: false),
+                    PublishDate = table.Column<string>(type: "TEXT", maxLength: 32, nullable: false),
+                    Published = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Author = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Posts_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Author", "CategoryId", "Content", "Excerpt", "PublishDate", "Published", "ThumbnailImagePath", "Title" },
+                values: new object[] { 1, "Oleg Donii", 1, "", "", "17.02.2022 11:47", true, "uploads/placeholder.jpg", "First post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Author", "CategoryId", "Content", "Excerpt", "PublishDate", "Published", "ThumbnailImagePath", "Title" },
+                values: new object[] { 2, "Oleg Donii", 2, "", "", "17.02.2022 11:47", true, "uploads/placeholder.jpg", "Second post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Author", "CategoryId", "Content", "Excerpt", "PublishDate", "Published", "ThumbnailImagePath", "Title" },
+                values: new object[] { 3, "Oleg Donii", 3, "", "", "17.02.2022 11:47", true, "uploads/placeholder.jpg", "Third post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Author", "CategoryId", "Content", "Excerpt", "PublishDate", "Published", "ThumbnailImagePath", "Title" },
+                values: new object[] { 4, "Oleg Donii", 1, "", "", "17.02.2022 11:47", true, "uploads/placeholder.jpg", "Fourth post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Author", "CategoryId", "Content", "Excerpt", "PublishDate", "Published", "ThumbnailImagePath", "Title" },
+                values: new object[] { 5, "Oleg Donii", 2, "", "", "17.02.2022 11:47", true, "uploads/placeholder.jpg", "Fifth post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Author", "CategoryId", "Content", "Excerpt", "PublishDate", "Published", "ThumbnailImagePath", "Title" },
+                values: new object[] { 6, "Oleg Donii", 3, "", "", "17.02.2022 11:47", true, "uploads/placeholder.jpg", "Six post" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_CategoryId",
+                table: "Posts",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
